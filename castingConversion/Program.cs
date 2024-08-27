e375a50090ca3dfce1eaa97131526452934880b5
ooMatter <Tyrice02@gmail.com>
2024-08-27 18:27:10 -0400

    Adding csating conversions complete challenges

---
+++ castingConversion/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+// int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+
+
+// Use the Casting coversion to convert an int to a string
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+
+// Convert a string to an int using the Parse method
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
+
+// Convert a string to a int using thr Convert class
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+
+// Tryparse() a string into anb int
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+*/
+
+ /*
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+// Stores the total of all the decimal values in the values array
+decimal total = 0m;
+
+// Stores a string of all the values that failed to convert to a decimal
+string message = string.Empty;
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        // if TryParse fails, add the string to the message
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total:C}");
+*/
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+// Convert.ToInt32() rounds up the way you would expect.
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
